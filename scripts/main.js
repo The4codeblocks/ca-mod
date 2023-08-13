1de2add4b847bde8e2d30cca148aff9c5ad8d326
The4codeblocks <the3spaceconstants@gmail.com>
2023-08-13 11:51:03 +0200

    Fix changed value

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,3 @@
 Events.on(EventType.ClientLoadEvent, () => {
-    Vars.mods.locateMod("sandbox-utilities").meta.description = "The Chaoticans\n[sky]T4[stat]SC [gray][Mass <=> [blue]Energy][][][][]";
+    Vars.mods.locateMod("sandbox-utilities").meta.author = "The Chaoticans\n[sky]T4[stat]SC [gray][Mass <=> [blue]Energy][][][][]";
 });
